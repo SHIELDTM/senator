e9aee7686e282db94f677544c2130342012175ff
zhityer <barcelona20202@gmail.com>
2015-12-06 17:08:29 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "youtube",
       "media_handler",
       "moderation"},
-    sudo_users = {102490691},
+    sudo_users = {128231895},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
